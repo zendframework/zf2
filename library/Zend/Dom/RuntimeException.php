5881cf723cea1fd43f62b6791c637a24a5f219d8
rettal <dan.latter@gmail.com>
2010-09-24 12:16:27 -0700

    Added new exception support to Zend\dom

--- library/Zend/Dom/RuntimeException.php
+++ library/Zend/Dom/RuntimeException.php
@@ -33,6 +33,6 @@ namespace Zend\Dom;
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class RuntimeException extends \Zend\Dom\Exception
+class RuntimeException extends \RuntimeException implements Exception
 {
 }
