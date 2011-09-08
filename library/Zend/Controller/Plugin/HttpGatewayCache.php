25f0c061ff52778e4b05524c6ec02af67a43eeb3
Christian Soronellas <csoronellas@emagister.com>
2011-09-08 21:15:39 +0200

    Added the HttpGatewayCache class as a front controller plugin. Modified the Http controller class in order to accept custom request headers.

---
+++ library/Zend/Controller/Plugin/HttpGatewayCache.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Zend\Controller\Plugin;
+
+/**
+ * A reverse proxy cache written on top of Zend Framework
+ *
+ * @author Christian Soronellas <christian@sistemes-cayman.es>
+ */
+class HttpGatewayCache extends AbstractPlugin
+{
+    /**
+     * The cache object instance
+     * @var Zend\Cache\Frontend
+     */
+    protected $_cache;
+    
+    /**
+     * A flag for whether the response comes already cached
+     * @var boolean
+     */
+    protected $_responseCached = false;
+
+    /**
+     * Class constructor
+     */
+    public function __construct(Zend\Cache\Frontend $cache)
+    {
+        $this->_cache = $cache;
+    }
+
+    /**
+     * Fired when the dispatch loop starts. Checks if the response is already cached.
+     */
+    public function dispatchLoopStartup(Request\AbstractRequest $request)
+    {
+        if (false !== ($data = $this->_cache->load($this->_getCacheKey($request)))) {
+            $this->_process($data);
+            $this->_responseCached = true;
+            $request->setDispatched(true);
+        }
+    }
+    
+    /**
+     * Fired when the dispatch loop has finished. Caches the response.
+     */
+    public function dispatchLoopShutdown()
+    {
+        // Here we have a full generated response, so replace ESI parts,
+        // cache the response, and go on.
+        if (!$this->_responseCached) {
+            $content = $this->getResponse()->getBody();
+            
+            // Get the "Cache-control" header and extract the value. This
+            // value will be the cached content lifetime. If no "Cache-control"
+            // header set, the content won't be cached.
+            foreach ($this->getResponse()->getHeaders() as $header) {
+                if ('cache-control' == strtolower($header['name'])) {
+                    // Extract the value, cache the content and go sleep! :)
+                    $lifetime = (int) sscanf(trim($header['value']), 'max-age=%d');
+                    $this->_cache->save($content, $this->_getCacheKey($request), array(), $lifetime);
+                }
+            }
+
+            $this->_process($content);
+        }
+    }
+    
+    /**
+     * Generates a cache key using the Request's path info
+     *
+     * @param Zend_Controller_Request_Abstract $request
+     * @return string
+     */
+    protected function _getCacheKey(Request\AbstractRequest $request)
+    {
+        return md5($request->getPathInfo());
+    }
+
+    /**
+     * Search for any esi include, and replace by its content
+     */
+    protected function _process($content)
+    {
+        if (strstr($content, '<esi:') !== false) {
+            $xml = simplexml_import_dom(\DOMDocument::loadHTML($content));
+            $xml->registerXPathNamespace('esi', 'http://www.edge-delivery.org/esi/1.0');
+            
+            // Check for esi includes, excluding those that its parent is another esi tag
+            foreach ($xml->xpath('//esi:include') as $esiInclude) {
+                
+            }
+            
+            $content = str_replace($match[0], $response->getBody(), $content);
+
+            $this->getResponse()->setBody($content);
+        }
+    }
+    
+    /**
+     * Try to process an ESI include by performing an internal request.
+     * Optionally it accepts an alternative URI for the cases when the
+     * first try returns an error.
+     * 
+     * @param string $src
+     * @param string $alt
+     * @return Zend\Controller\Response\Http
+     */
+    protected function _processInclude($src, $alt = null)
+    {
+        $response = $this->_performInternalEsiRequest($src);
+        
+        // Check the response for any errors
+        if (400 >= $response->getHTTPResponseCode()) {
+            // Try to reach the alternate page if present
+            if (null !== $alt) {
+                $response = $this->_performInternalEsiRequest($alt);
+            }
+        }
+        
+        return $response;
+    }
+    
+    protected function _performInternalEsiRequest($uri)
+    {
+        $frontController = Front::getInstance();
+        $frontController->returnResponse(true);
+        
+        // Try to reach the the URI specified at src param
+        $request = new Request\Http(parse_url($src, PHP_URL_PATH));
+        $request->addHeader('Surrogate-Capability', 'ZendHttpCacheGateway/1.0');
+        $response = $frontController->dispatch($request);
+        
+        $frontController->returnResponse(false);
+    }
+}
