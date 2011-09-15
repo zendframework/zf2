4d2793eb9f9268381e242d5294d8686d930ca1a7
Christian Soronellas <csoronellas@emagister.com>
2011-09-15 08:23:23 +0200

    Fixed the plugin file and class annotations

--- library/Zend/Controller/Plugin/HttpGatewayCache.php
+++ library/Zend/Controller/Plugin/HttpGatewayCache.php
@@ -1,7 +1,28 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Controller
+ * @subpackage Plugins
+ * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 
+/**
+ * @namespace
+ */
 namespace Zend\Controller\Plugin;
-
 use Zend\Controller,
     Zend\Controller\Request,
     Zend\Controller\Response;
@@ -9,7 +30,15 @@ use Zend\Controller,
 /**
  * A reverse proxy cache written on top of Zend Framework
  *
- * @author Christian Soronellas <christian@sistemes-cayman.es>
+ * @uses       \Zend\Controller\Plugin\AbstractPlugin
+ * @uses       \Zend\Cache\Frontend
+ * @uses       \Zend\Controller\Request\AbstractRequest
+ * @uses       \Zend\Controller\Response\AbstractResponse
+ * @category   Zend
+ * @package    Zend_Controller
+ * @subpackage Plugins
+ * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class HttpGatewayCache extends AbstractPlugin
 {
