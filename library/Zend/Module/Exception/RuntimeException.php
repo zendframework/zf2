8c89262923741d7353d068ca339dbad57ef846b9
Kyle Spraggs <kyle.spraggs@avimark.net>
2011-11-08 21:09:04 -0600

    Updated Exception to conform to cs.

--- library/Zend/Module/Exception/RuntimeException.php
+++ library/Zend/Module/Exception/RuntimeException.php
@@ -1,6 +1,7 @@
 <?php
 namespace Zend\Module\Exception;
 
-class RuntimeException extends \RuntimeException
-{
-}
+class RuntimeException 
+    extends \RuntimeException
+    implements \Zend\Module\Exception
+{}
