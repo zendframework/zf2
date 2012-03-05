52fca0461265c697981245cbd9381636c303260c
Matt Pinkston <matt@pinkston.org>
2012-03-04 19:08:25 -0600

    Method of asserting type before Di instantiation
    This feature provides a possible method of asserting a resource type
    before it is instantiated by the Dependency Injector.
    
    Since it is possible for modules to alter each other's configs (afaik),
    I wanted a way to ensure the resource I'm requesting is what I'll actually
    get.
    
    I updated Mvc/Application to use this feature in locating the controller.
    This allows us to ensure the requested resource is Dispatchable before
    it's instantiated.
    
    It uses the existing definition list where possible, so there shouldn't
    be much of a performance hit. (Assuming I didn't miss something big..)

---
+++ library/Zend/Di/Assertion.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zend\Di;
+use Zend\Di\Definition\Definition;
+
+/**
+ * Assertion interface.
+ */
+interface Assertion
+{
+	/**
+	 * assert function.
+	 * 
+	 * @access public
+	 * @param mixed $class
+	 * @param mixed Definition $definition. (default: null)
+	 * @return void
+	 */
+	public function assert($class, Definition $definition = null);
+
+	/**
+	 * __toString function.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __toString();
+}
