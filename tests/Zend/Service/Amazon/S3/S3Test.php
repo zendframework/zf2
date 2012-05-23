a7f2a1b78c00ba10ff90462397f21b951e76d914
Duccio Gasparri <duccio.gasparri@getlocal.it>
2012-05-23 11:51:28 +0200

    fixed an error in S3Test.php

--- tests/Zend/Service/Amazon/S3/S3Test.php
+++ tests/Zend/Service/Amazon/S3/S3Test.php
@@ -149,6 +149,7 @@ BODY;
     {    	 
         //Valid bucket name
         $bucket   = 'iamavalidbucket';
+        $location = '';
         $this->amazon->setCurrentKeys('AKIAIDCZ2WXN6NNB7YZA','sagA0Lge8R+ifORcyb6Z/qVbmtimFCUczvh51Jq8'); //Fake keys
         $this->amazon->setRequestDate("Tue, 15 May 2012 15:18:31 +0000");
         
