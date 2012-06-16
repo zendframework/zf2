6093ca81214d6a42bd562d723bfe710aeae934d9
Evan Coury <me@evancoury.com>
2012-06-16 04:52:15 -0700

    Remove typehint on ResultSet row object prototype params
    This causes a strict standards issue with the HydratingResultSet.

--- library/Zend/Db/ResultSet/ResultSet.php
+++ library/Zend/Db/ResultSet/ResultSet.php
@@ -69,7 +69,7 @@ class ResultSet implements Countable, Iterator /*, ResultSetInterface */
      * @param  null|RowObjectInterface $rowObjectPrototype 
      * @return void
      */
-    public function __construct(RowObjectInterface $rowObjectPrototype = null)
+    public function __construct($rowObjectPrototype = null)
     {
         $this->setRowObjectPrototype(($rowObjectPrototype) ?: new Row);
     }
@@ -80,7 +80,7 @@ class ResultSet implements Countable, Iterator /*, ResultSetInterface */
      * @param  RowObjectInterface $rowObjectPrototype 
      * @return ResultSet
      */
-    public function setRowObjectPrototype(RowObjectInterface $rowObjectPrototype)
+    public function setRowObjectPrototype($rowObjectPrototype)
     {
         $this->rowObjectPrototype = $rowObjectPrototype;
         return $this;
