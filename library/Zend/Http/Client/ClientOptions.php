87abb1c15b75efb15d50d5cb7f479c5280af9c28
Spabby <its.spabby@gmail.com>
2012-07-02 10:03:08 +0100

    More work on ClientOptions.php

--- library/Zend/Http/Client/ClientOptions.php
+++ library/Zend/Http/Client/ClientOptions.php
@@ -6,5 +6,10 @@ use Zend\Http\Request;
 
 class ClientOptions extends AbstractOptions
 {
-
+    /**
+     * Maximum number of redirects to follow
+     *
+     * @var int
+     */
+    protected $maxRedirects = 5;
 }
