67423f2198b1d4f3a132885ff90a709197cc4988
Gary Hockin <gary@hock.in>
2012-07-02 12:29:25 +0100

    More work on ClientOptions.php

--- library/Zend/Http/Client/ClientOptions.php
+++ library/Zend/Http/Client/ClientOptions.php
@@ -26,4 +26,63 @@ class ClientOptions extends AbstractOptions
      * @var string
      */
     protected $userAgent = 'Zend\Http\Client';
+
+    /**
+     * Connection timeout in seconds
+     *
+     * @var int
+     */
+    protected $timeout = 10;
+
+    /**
+     * Connection adapter class to use
+     *
+     * @var string
+     */
+    protected $adapter = 'Zend\Http\Client\Adapter\Socket';
+
+    /**
+     * User agent identifier string that is sent in the request headers
+     *
+     * @var string
+     */
+    protected $httpVersion = Request::VERSION_11;
+
+    /**
+     * Whether to store last response for later retrieval
+     *
+     * @var bool
+     */
+    protected $storeResponse = true;
+
+    /**
+     * Whether to enable keep-alive connections with the server.
+     * Useful and might improve performance if several consecutive requests to the same server are performed.
+     *
+     * @var bool
+     */
+    protected $keepAlive = false;
+
+    /**
+     * The name of a file to stream output to, or false to disable
+     *
+     * @var string|bool
+     */
+    protected $outputStream = false;
+
+    /**
+     * Whether to pass the cookie value through urlencode/urldecode.
+     * Enabling this breaks support with some web servers.
+     * Disabling this limits the range of values the cookies can contain.
+     *
+     * @var bool
+     */
+    protected $encodeCookies = true;
+
+    /**
+     * Whether URIs need to strictly adhere to the RFC 3986 uri scheme
+     *
+     * @var bool
+     */
+    protected $rfc3986Strict = false;
 }
