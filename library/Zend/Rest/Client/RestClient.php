467e0f0ff80da745bde73046b06bf5026a90dfdb
John Kelly <wablam@gmail.com>
2012-07-26 14:20:20 -0700

    Make Rest tests pass

--- library/Zend/Rest/Client/RestClient.php
+++ library/Zend/Rest/Client/RestClient.php
@@ -90,8 +90,8 @@ class RestClient extends \Zend\Service\AbstractService
     /**
      * Call a remote REST web service URI and return the Zend_Http_Response object
      *
-     * @param  string $path            The path to append to the URI
-     * @throws Zend\Rest\Client\Exception\UnexpectedValueException
+     * @param  string $path The path to append to the URI
+     * @throws Exception\UnexpectedValueException
      * @return void
      */
     final private function _prepareRest($path)
@@ -114,7 +114,9 @@ class RestClient extends \Zend\Service\AbstractService
          * each time as the Zend\Http\Client instance may be shared with other 
          * Zend\Service\AbstractService subclasses.
          */
-        $this->getHttpClient()->resetParameters()->setUri($this->_uri);
+        $client = $this->getHttpClient();
+        $client->resetParameters();
+        $client->setUri($this->_uri);
     }
 
     /**
@@ -125,12 +127,14 @@ class RestClient extends \Zend\Service\AbstractService
      * @throws Zend\Http\Client\Exception
      * @return Zend\Http\Response
      */
-    final public function restGet($path, array $query = null)
+    final public function restGet($path, array $query = array())
     {
         $this->_prepareRest($path);
         $client = $this->getHttpClient();
         $client->setParameterGet($query);
-        return $client->request('GET');
+        $client->setMethod(\Zend\Http\Request::METHOD_GET);
+        $response = $client->send();
+        return $response;
     }
 
     /**
@@ -148,11 +152,12 @@ class RestClient extends \Zend\Service\AbstractService
     {
         $client = $this->getHttpClient();
         if (is_string($data)) {
-            $client->setRawData($data);
+            $client->setRawBody($data);
         } elseif (is_array($data) || is_object($data)) {
             $client->setParameterPost((array) $data);
         }
-        return $client->request($method);
+        $client->setMethod($method);
+        return $client->send();
     }
 
     /**
@@ -193,7 +198,7 @@ class RestClient extends \Zend\Service\AbstractService
     final public function restDelete($path)
     {
         $this->_prepareRest($path);
-        return $this->getHttpClient()->request('DELETE');
+        return $this->getHttpClient()->setMethod('DELETE')->send();
     }
 
     /**
--- tests/Zend/Rest/RouteTest.php
+++ tests/Zend/Rest/RouteTest.php
@@ -107,7 +107,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('index', $values['controller']);
         $this->assertEquals('index', $values['action']);
     }
@@ -122,7 +122,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('user', $values['controller']);
         $this->assertEquals('index', $values['action']);
     }
@@ -134,7 +134,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('user', $values['controller']);
         $this->assertEquals('index', $values['action']);
     }
@@ -146,7 +146,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('user', $values['controller']);
         $this->assertEquals('index', $values['action']);
         $this->assertEquals(123456789, $values['changedSince']);
@@ -160,7 +160,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('user', $values['controller']);
         $this->assertEquals('index', $values['action']);
         $this->assertEquals(123456789, $values['changedSince']);
@@ -174,7 +174,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('project', $values['controller']);
         $this->assertEquals('get', $values['action']);
         $this->assertEquals('zendframework', $values['id']);
@@ -187,7 +187,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('project', $values['controller']);
         $this->assertEquals('get', $values['action']);
         $this->assertEquals('zendframework', $values['id']);
@@ -200,7 +200,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('project', $values['controller']);
         $this->assertEquals('get', $values['action']);
         $this->assertEquals('zend framework', $values['id']);
@@ -213,7 +213,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType('array', $values);
         $this->assertTrue(isset($values['module']));
-        $this->assertEquals('default', $values['module']);
+        $this->assertEquals('application', $values['module']);
         $this->assertEquals('project', $values['controller']);
         $this->assertEquals('edit', $values['action']);
         $this->assertEquals('zendframework', $values['id']);
@@ -311,7 +311,7 @@ class RouteTest extends \PHPUnit_Framework_TestCase
 
         $routedRequest = $router->route($request);
 
-        $this->assertEquals("default", $routedRequest->getParam("module"));
+        $this->assertEquals("application", $routedRequest->getParam("module"));
         $this->assertEquals("user", $routedRequest->getParam("controller"));
         $this->assertEquals("get", $routedRequest->getParam("action"));
         $this->assertEquals("lcrouch", $routedRequest->getParam("id"));
