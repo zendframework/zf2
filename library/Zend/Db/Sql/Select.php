8008a1e51f5f43ce29f0bd0ca5cfc195abca9996
Magnus Berglund <magnus@auforia.com>
2012-08-26 17:23:20 +0200

    Cleaned up fix for issue ZF2-503

--- library/Zend/Db/Sql/Select.php
+++ library/Zend/Db/Sql/Select.php
@@ -703,12 +703,10 @@ class Select extends AbstractSql implements SqlInterface, PreparableSqlInterface
             $sql = $driver->formatParameterName('limit');
             $parameterContainer->offsetSet('limit', $this->limit, ParameterContainer::TYPE_INTEGER);
         } else {
-			if (is_int($this->limit)) {
-				$sql = $this->limit;
-			}
-			else {
-				throw new Exception\InvalidArgumentException('LIMIT should be given as INTEGER');
-			}
+            if (!is_int($this->limit)) {
+                throw new Exception\InvalidArgumentException('LIMIT should be given as INTEGER');
+            }
+            $sql = $this->limit;
         }
         return array($sql);
     }
@@ -722,12 +720,10 @@ class Select extends AbstractSql implements SqlInterface, PreparableSqlInterface
             $parameterContainer->offsetSet('offset', $this->offset, ParameterContainer::TYPE_INTEGER);
             return array($adapter->getDriver()->formatParameterName('offset'));
         } else {
-			if (is_int($this->offset)) {
-				return array($this->offset);
-			}
-			else {
-				throw new Exception\InvalidArgumentException('OFFSET should be given as INTEGER');
-			}
+            if (!is_int($this->offset)) {
+                throw new Exception\InvalidArgumentException('OFFSET should be given as INTEGER');							
+            }	
+            return array($this->offset);
         }
     }
 
