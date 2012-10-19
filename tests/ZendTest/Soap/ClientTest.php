609afb5822b4c79db3a0ed3b784c584b6e99e9b8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2012-10-19 17:03:52 +0700

    remove disable cache to make outdated diff show, no need to add cache for travis build

--- tests/ZendTest/Soap/ClientTest.php
+++ tests/ZendTest/Soap/ClientTest.php
@@ -491,7 +491,6 @@ class ClientTest extends \PHPUnit_Framework_TestCase
     
     public function testSoapCallFunc()
     {
-        ini_set("soap.wsdl_cache_enabled", 0); 
         $client = new Client(__DIR__ . '/TestAsset/Soap.wsdl');
         
         // call
