59313e89c600278a23cb7d09881a959b3b028965
Daniel Gimenes <daniel@coremax.com.br>
2013-01-16 18:36:54 -0300

    Fix #3437: Fixed usage of Traverseable for $options.

--- library/Zend/Form/Element/Captcha.php
+++ library/Zend/Form/Element/Captcha.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  * @package   Zend_Form
  */
@@ -32,15 +32,15 @@ class Captcha extends Element implements InputProviderInterface
      * Accepted options for Captcha:
      * - captcha: a valid Zend\Captcha\AdapterInterface
      *
-     * @param array|\Traversable $options
+     * @param  array|\Traversable $options
      * @return Captcha
      */
     public function setOptions($options)
     {
         parent::setOptions($options);
 
-        if (isset($options['captcha'])) {
-            $this->setCaptcha($options['captcha']);
+        if (isset($this->options['captcha'])) {
+            $this->setCaptcha($this->options['captcha']);
         }
 
         return $this;
