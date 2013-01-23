68ae9bfb5e820a9878ff920fb9c04152d25e1e67
prolic <saschaprolic@googlemail.com>
2013-01-23 12:37:46 +0100

    undo unneeded refactor

--- library/Zend/Barcode/Renderer/Image.php
+++ library/Zend/Barcode/Renderer/Image.php
@@ -216,10 +216,10 @@ class Image extends AbstractRenderer
         } else {
             $width = $barcodeWidth;
             $height = $barcodeHeight;
-            if ($this->userWidth && 'error' != $this->barcode->getType()) {
+        if ($this->userWidth && $this->barcode->getType() != 'error') {
                 $width = $this->userWidth;
             }
-            if ($this->userHeight && 'error' != $this->barcode->getType()) {
+            if ($this->userHeight && $this->barcode->getType() != 'error') {
                 $height = $this->userHeight;
             }
 
