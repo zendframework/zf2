7d3acae4649b846e4414d99e0b03c424ff72f931
tux-rampage <staff@pro-unreal.de>
2013-04-25 18:00:24 +0200

    Changed leaf comparsion in Zend\Permissions\Rbac\Rbac::getRole()
    Changed this from simple compare with `==` to exact match operator `===`
    which fits better for comparing objects.

--- library/Zend/Permissions/Rbac/Rbac.php
+++ library/Zend/Permissions/Rbac/Rbac.php
@@ -166,7 +166,7 @@ class Rbac extends AbstractIterator
 
         $it = new RecursiveIteratorIterator($this, RecursiveIteratorIterator::CHILD_FIRST);
         foreach ($it as $leaf) {
-            if ((is_string($objectOrName) && $leaf->getName() == $objectOrName) || $leaf == $objectOrName) {
+            if ((is_string($objectOrName) && $leaf->getName() == $objectOrName) || $leaf === $objectOrName) {
                 return $leaf;
             }
         }
