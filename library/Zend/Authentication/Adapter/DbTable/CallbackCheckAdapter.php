d40d8e3d0fcffa6e3ffecbaced2458ff5e559a4d
Jaime Baez <jbaez@dict8.com>
2013-05-14 17:53:07 +0100

    swap order of the arguments in credentialValidationCallback

--- library/Zend/Authentication/Adapter/DbTable/CallbackCheckAdapter.php
+++ library/Zend/Authentication/Adapter/DbTable/CallbackCheckAdapter.php
@@ -96,7 +96,7 @@ class CallbackCheckAdapter extends AbstractAdapter
     protected function authenticateValidateResult($resultIdentity)
     {
         try {
-            $callbackResult = call_user_func($this->credentialValidationCallback, $resultIdentity[$this->credentialColumn], $this->credential);
+            $callbackResult = call_user_func($this->credentialValidationCallback, $this->credential, $resultIdentity[$this->credentialColumn]);
         } catch (\Exception $e) {
             $this->authenticateResultInfo['code']       = AuthenticationResult::FAILURE_UNCATEGORIZED;
             $this->authenticateResultInfo['messages'][] = $e->getMessage();
