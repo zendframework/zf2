38c83e3945f56be9578fe21e875ae1a5706a1fe2
Gounlaf <florianlevis@Trillian.local>
2013-08-11 14:37:34 +0200

    Fix setLabel() to add label in option
    Noticed that setLabel doesn't put label in options ; so when calling
    getOption('label') it doesn't work

--- library/Zend/Form/Element.php
+++ library/Zend/Form/Element.php
@@ -314,6 +314,7 @@ class Element implements
     {
         if (is_string($label)) {
             $this->label = $label;
+            $this->options['label'] = $label;
         }
 
         return $this;
