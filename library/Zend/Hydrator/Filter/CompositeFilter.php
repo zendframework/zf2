425d5c4d5d25bf227e66b3649152182c7ba4930f
Michaël Gallego <mic.gallego@gmail.com>
2013-08-22 11:43:12 +0200

    Optimize filter composite

--- library/Zend/Hydrator/Filter/CompositeFilter.php
+++ library/Zend/Hydrator/Filter/CompositeFilter.php
@@ -21,23 +21,106 @@ class CompositeFilter implements FilterInterface
     const CONDITION_AND = 2;
 
     /**
-     * @var array
-     */
-    protected $orFilters;
-
-    /**
-     * @var array
-     */
-    protected $andFilters;
-
-    /**
-     * Should return true to extract the given property, false otherwise
+     * The type of this composite filter
      *
-     * @param  string $property The name of the property
-     * @return bool
+     * @var int
+     */
+    protected $type;
+
+    /**
+     * @var array|FilterInterface[]
+     */
+    protected $filters;
+
+    /**
+     * Constructor
+     *
+     * @param int $type
+     */
+    public function __construct($type = self::CONDITION_OR)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Set the type of the composite filter
+     *
+     * @param int $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get the type of the composite filter (CONDITION_OR or CONDITION_AND)
+     *
+     * @return int
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Add a new filter to the composite filter
+     *
+     * @param  FilterInterface $filter
+     * @return void
+     */
+    public function addFilter(FilterInterface $filter)
+    {
+        $this->filters[] = $filter;
+    }
+
+    /**
+     * Remove a filter from the composite filter
+     *
+     * Note that this method needs to iterate through each filters,
+     * which can be expensive if there are a lot
+     *
+     * @param  FilterInterface $filter
+     * @return bool Returns true if the filter has been removed, false otherwise
+     */
+    public function removeFilter(FilterInterface $filter)
+    {
+        foreach ($this->filters as $key => $currentFilter) {
+            if ($filter === $currentFilter) {
+                unset($this->filters[$key]);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
      */
     public function filter($property)
     {
+        // If there are no filters attached, we decide to filter
+        if (empty($this->filters)) {
+            return true;
+        }
+
+        // If condition is OR, only one filter needs to evaluate to true
+        if ($this->type === self::CONDITION_OR) {
+            foreach ($this->filters as $filter) {
+                if ($filter->filter($property) === true) {
+                    return true;
+                }
+            }
+        }
+
+        // If condition is AND, all the filters need to evaluate to true
+        foreach ($this->filters as $filter) {
+            if ($filter->filter($property) === false) {
+                return false;
+            }
+        }
+
+        // If we're here, then all filters evaluated to true
         return true;
     }
 }
