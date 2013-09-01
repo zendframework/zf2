37fbea45134bd18d1d16cc49bf8ad264ee456024
Michaël Gallego <mic.gallego@gmail.com>
2013-09-01 19:35:33 +0200

    CS fixes

--- tests/ZendTest/Stdlib/ProvidesOptionsTraitTest.php
+++ tests/ZendTest/Stdlib/ProvidesOptionsTraitTest.php
@@ -5,7 +5,6 @@
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Stdlib
  */
 
 namespace ZendTest\Stdlib;
@@ -14,10 +13,7 @@ use Zend\Stdlib\SplPriorityQueue;
 use ZendTest\Stdlib\TestAsset\TestOptionsTrait;
 
 /**
- * @category   Zend
- * @package    Zend_Stdlib
- * @subpackage UnitTests
- * @group      Zend_Stdlib
+ * @group Zend_Stdlib
  */
 class ProvidesOptionsTraitTest extends \PHPUnit_Framework_TestCase
 {
