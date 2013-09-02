711f3086d29a0f9a97f05ff5c8c9603689057545
Stefano Sala <stefano.sala@spa.it>
2013-09-02 09:20:10 +0200

    Added failing test to reproduce the error with reflection and core classes.

--- tests/ZendTest/Code/Reflection/MethodReflectionTest.php
+++ tests/ZendTest/Code/Reflection/MethodReflectionTest.php
@@ -59,4 +59,10 @@ class MethodReflectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("    {\n\n        return 'mixedValue';\n\n    }\n", $reflectionMethod->getContents(false));
     }
 
+    public function testGetContentsWithCoreClass()
+    {
+        $reflectionMethod = new MethodReflection('DateTime', 'format');
+        $this->assertEquals("???", $reflectionMethod->getContents(false));
+    }
+
 }
