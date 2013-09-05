237e36aea0b16513229b9a8a7e8896d012ed3941
Michaël Gallego <mic.gallego@gmail.com>
2013-09-05 20:10:55 +0200

    Try validator chain

--- library/Zend/Validator/ValidatorChain.php
+++ library/Zend/Validator/ValidatorChain.php
@@ -11,8 +11,15 @@ namespace Zend\Validator;
 
 use Countable;
 use Zend\Stdlib\PriorityQueue;
+use Zend\Stdlib\SplPriorityQueue;
 
-class ValidatorChain extends AbstractValidator implements Countable
+/**
+ * A validator chain that allows to execute multiple validators one after the other
+ *
+ * Accepted options are:
+ *      - validators
+ */
+class ValidatorChain implements ValidatorInterface, Countable
 {
     /**
      * Default priority at which validators are added
@@ -88,12 +95,22 @@ class ValidatorChain extends AbstractValidator implements Countable
      * Attach a validator to the chain by its name (using the validator plugin manager)
      *
      * @param  string $name Valid name
+     * @param  array  $options Optional options
      * @param  int    $priority Priority at which to enqueue validator; defaults to 1 (higher executes earlier)
      * @return void
      */
-    public function attachByName($name, $priority = self::DEFAULT_PRIORITY)
+    public function attachByName($name, array $options = array(), $priority = self::DEFAULT_PRIORITY)
     {
+        // @TODO: if we somewhat formalize the concept of options, we should be able to have a second
+        // parameter for each plugin manager, which would be option, and the plugin manager would
+        // automatically inject options for us
+
         $validator = $this->validatorPluginManager->get($name);
+
+        if (method_exists($validator, 'setOptions')) {
+            $validator->setOptions($options);
+        }
+
         $this->validators->insert($validator, $priority);
     }
 
@@ -107,6 +124,28 @@ class ValidatorChain extends AbstractValidator implements Countable
     {
         foreach ($validatorChain->validators->toArray(PriorityQueue::EXTR_BOTH) as $item) {
             $this->attach($item['data'], $item['priority']);
+        }
+    }
+
+    /**
+     * Set validators using concrete instances or specification
+     *
+     * @param array|ValidatorInterface[] $validators
+     */
+    public function setValidators(array $validators)
+    {
+        $this->validators = new SplPriorityQueue();
+
+        // @TODO: should specification be handled here or should we provide a factory?
+        foreach ($validators as $validator) {
+            if ($validator instanceof ValidatorInterface) {
+                $this->attach($validator);
+            } elseif (is_array($validator)) {
+                $options  = isset($validator['options']) ? $validator['options'] : array();
+                $priority = isset($validator['priority']) ? $validator['priority'] : self::DEFAULT_PRIORITY;
+
+                $this->attachByName($validator['name'], $options, $priority);
+            }
         }
     }
 
@@ -125,9 +164,14 @@ class ValidatorChain extends AbstractValidator implements Countable
      *
      * {@inheritDoc}
      */
-    public function validate($value)
+    public function validate($data, $context = null)
     {
-        // @TODO
+        // @TODO: fix this
+        foreach ($this->validators as $validator) {
+            $validationResult = $validator->validate($data);
+        }
+
+        return $validationResult;
     }
 
     /**
