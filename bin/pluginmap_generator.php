c5bafe25f9094c9dddcf7085a50750457ac841b5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2013-10-03 23:42:03 +0700

    cleaning @category @package @subpackage in all resources continue #5215 and #3508

--- bin/pluginmap_generator.php
+++ bin/pluginmap_generator.php
@@ -4,9 +4,6 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @category   Zend
- * @package    Zend_Loader
- * @subpackage Exception
  * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
