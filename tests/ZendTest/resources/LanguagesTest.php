892d5bc51f2d1508925a8ddcbff26d1dd4deaff0
Test <test@example.com>
2013-10-25 13:59:06 +0200

    Add test for resources + polish plural form

---
+++ tests/ZendTest/resources/LanguagesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class LanguagesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLanguageResourcesHavePluralFormSet()
+    {
+        $langResPath = __DIR__ . '/../../../resources/languages';
+        $ri = new \RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($langResPath)
+        );
+        while($ri->valid()) {
+            if (!$ri->isDot() && strpos($ri->key(), '.php')!==false) {
+                $result = include $ri->key();
+                $this->assertArrayHasKey('', $result, 'Config sub-array not set in '. $ri->getSubPathName());
+                $this->assertArrayHasKey('plural_forms', $result[''], 'Plural forms not set in ' . $ri->getSubPathName());
+            }
+
+            $ri->next();
+        }
+    }
+}
