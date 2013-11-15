1e66ecfcf5a2b75e5e34600a3bf8735c64f29535
Michaël Gallego <mic.gallego@gmail.com>
2013-11-15 10:18:17 +0100

    Sort numerically

--- demos/index.php
+++ demos/index.php
@@ -10,28 +10,28 @@ $eventManager  = new \Zend\EventManager\EventManager(array('myid'));
 $sharedManager = new \Zend\EventManager\SharedEventManager();
 
 $eventManager->setSharedManager($sharedManager);
-//$eventManager->attach('event', function() { echo 'hello'; }, 3);
-//$eventManager->attach('event', function() { echo 'hello'; }, 3);
+$eventManager->attach('event', function() { echo 'hello'; }, 5);
+$eventManager->attach('event', function() { echo 'world'; }, 50);
 //$sharedManager->attach('myid', 'event', function() { echo 'world'; });
 //$eventManager->attach('event', function() { echo 'hello'; }, 3);
 
-//$eventManager->trigger('event');
+$eventManager->trigger('event');
 
 
-for ($i = 0 ; $i != 50 ; ++$i) {
+/*for ($i = 0 ; $i != 50 ; ++$i) {
     $eventManager->attach('event', function() {}, $i);
     $sharedManager->attach('myid', 'event', function() {}, $i);
-}
+}*/
 
 //$eventManager->attach('event', function() {}, 2);
 
 //$eventManager->attach('event', function(){}, 1);
-
+/*
 for ($i=0 ; $i<$nb_occur; $i++)
 {
     $eventManager->trigger('event');
 }
-
+*/
 $time_end = microtime(true);
 $time = $time_end - $time_start;
 
