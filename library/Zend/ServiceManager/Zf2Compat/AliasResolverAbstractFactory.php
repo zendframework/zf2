c5bf50f0906da665f61d3cea688b5424cab89c5b
Marco Pivetta <ocramius@gmail.com>
2013-11-18 23:15:49 +0100

    Stubbing out alias ZF3 -> ZF2 compatibility abstract factory

---
+++ library/Zend/ServiceManager/Zf2Compat/AliasResolverAbstractFactory.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\ServiceManager\Zf2Compat;
+
+use Zend\ServiceManager\AbstractFactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Alias abstract factory - allows usage of aliases in {@see \Zend\ServiceManager\ServiceManager} even
+ * though they are no more a first-class citizien of the component.
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ */
+class AliasResolverAbstractFactory implements AbstractFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        // TODO: Implement canCreateServiceWithName() method.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        // TODO: Implement createServiceWithName() method.
+    }
+}
