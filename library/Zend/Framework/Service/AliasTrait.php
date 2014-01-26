f1c60904077110b3d58d7d903ef42b5c80e69d56
devosc <devosc@gmail.com>
2014-01-25 23:22:54 -0600

    alias trait

--- library/Zend/Framework/Service/AliasTrait.php
+++ library/Zend/Framework/Service/AliasTrait.php
@@ -47,7 +47,7 @@ trait AliasTrait
      */
     public function get($name, array $options = [])
     {
-        return $this->sm->request(new Request($name, $this->alias($name), $options));
+        return $this->sm->get($this->alias($name), $options);
     }
 
     /**
