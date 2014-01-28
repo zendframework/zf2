11a6bf8f257341a48b9791c6754c9e73e66eae32
devosc <devosc@gmail.com>
2014-01-27 22:24:47 -0600

    ci

--- library/Zend/Framework/Service/RequestInterface.php
+++ library/Zend/Framework/Service/RequestInterface.php
@@ -12,11 +12,6 @@ namespace Zend\Framework\Service;
 interface RequestInterface
 {
     /**
-     *
-     */
-    const EVENT_SERVICE = 'service';
-
-    /**
      * @return string
      */
     public function alias();
