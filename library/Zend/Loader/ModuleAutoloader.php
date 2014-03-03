ce6e8825dffa830e1b5735f32ac6dedc8ea844ca
James Newell <james@digitaledgeit.com.au>
2014-03-04 07:37:37 +1100

    Moved the assignment out of the conditional

--- library/Zend/Loader/ModuleAutoloader.php
+++ library/Zend/Loader/ModuleAutoloader.php
@@ -186,7 +186,8 @@ class ModuleAutoloader implements SplAutoloader
             $path = $path . $moduleClassPath;
 
             if ($path == '.' || substr($path, 0, 2) == './' || substr($path, 0, 2) == '.\\') {
-                if (($basePath = realpath('.')) === false) {
+                $basePath = realpath('.');
+                if ($basePath === false) {
                     $basePath = getcwd();
                 }
                 $path = rtrim($basePath, '\/\\') . substr($path, 1);
