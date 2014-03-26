b352c9e92ed7b2fe27921ddc76bea5b5ef77c6e0
Bruce Lampson <brucelampson@gmail.com>
2014-03-26 01:36:53 -0600

    Suggest feature
    I am currently working on a project on which one of the many requirements was to associate the user's telephone country code according to their location country code. thanks to ZF2 this was a breeze and i will like to suggest this method.

--- library/Zend/I18n/Validator/PhoneNumber.php
+++ library/Zend/I18n/Validator/PhoneNumber.php
@@ -40,6 +40,11 @@ class PhoneNumber extends AbstractValidator
     protected static $phone = array();
 
     /**
+     * @var
+     */
+    protected $code;
+
+    /**
      * ISO 3611 Country Code
      *
      * @var string
@@ -131,6 +136,22 @@ class PhoneNumber extends AbstractValidator
         }
 
         return $this->allowPossible;
+    }
+
+
+    /**
+     * Get Telephone Country Code
+     * 
+     * @return string
+     */
+    public function getCode()
+    {
+        if (null === $this->code) {
+            $pattern = $this->loadPattern($this->getCountry());
+            $this->code = $pattern['code'];
+        }
+
+        return $this->code;
     }
 
     /**
