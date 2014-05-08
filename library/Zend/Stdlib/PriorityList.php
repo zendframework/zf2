79a3191d03054336b6b6dc9bc0e97bee64bd4366
turrsis <turrsis@gmail.com>
2014-05-08 21:01:15 +0300

    fix performance

--- library/Zend/Stdlib/PriorityList.php
+++ library/Zend/Stdlib/PriorityList.php
@@ -197,7 +197,7 @@ class PriorityList implements Iterator, Countable
      */
     public function current()
     {
-        $this->sort();
+        $this->sorted || $this->sort();
         $node = current($this->items);
 
         return $node ? $node['data'] : false;
@@ -208,6 +208,7 @@ class PriorityList implements Iterator, Countable
      */
     public function key()
     {
+        $this->sorted || $this->sort();
         return key($this->items);
     }
 
