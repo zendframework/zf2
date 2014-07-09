970adcd2a4b41b5e3ef22242e4d706ee2eeaab1c
Webdevilopers <code@webdevilopers.net>
2014-07-09 12:51:27 +0200

    Update CollectionTest.php

--- tests/ZendTest/Form/Element/CollectionTest.php
+++ tests/ZendTest/Form/Element/CollectionTest.php
@@ -1030,7 +1030,7 @@ class CollectionTest extends TestCase
         $this->assertCount(2, $form->get('names'));
     }
 
-    public function testMininumLenghtIsMaintanedWhenSettingAnSmallerCollection()
+    public function testMininumLenghtIsMaintanedWhenSettingASmallerCollection()
     {
         $arrayCollection = array(
             new Element\Color(),
