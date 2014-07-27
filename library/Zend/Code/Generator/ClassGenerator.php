c76e496e7646bcd57280ce6afbfd36495de8abf6
Marco Pivetta <ocramius@gmail.com>
2014-07-27 23:08:13 +0200

    #6288 - clarifying on returned indexes

--- library/Zend/Code/Generator/ClassGenerator.php
+++ library/Zend/Code/Generator/ClassGenerator.php
@@ -457,7 +457,7 @@ class ClassGenerator extends AbstractGenerator
     }
 
     /**
-     * @return PropertyGenerator[]
+     * @return PropertyGenerator[] indexed by constant name
      */
     public function getConstants()
     {
