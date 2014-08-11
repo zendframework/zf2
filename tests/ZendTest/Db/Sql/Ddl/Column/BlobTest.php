4454388c89781d46697928ad3de945ad9a9f8438
Oleg <olegkulik1985@gmail.com>
2014-08-11 11:24:37 +0300

    - fix some column definitions (blob, text, integer, bigint, varchar, char) - add column def (datetime) - add Index constraint type - add AlterTableDecorator for Platform - adjust tests to be compatible with new definitions

--- tests/ZendTest/Db/Sql/Ddl/Column/BlobTest.php
+++ tests/ZendTest/Db/Sql/Ddl/Column/BlobTest.php
@@ -14,6 +14,29 @@ use Zend\Db\Sql\Ddl\Column\Blob;
 class BlobTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * blob does not support length
+     * @covers Zend\Db\Sql\Ddl\Column\Blob::setLength
+     * @deprecated
+     */
+    public function testSetLength()
+    {
+        $blob = new Blob('foo', 55);
+        $this->assertEquals(55, $blob->getLength());
+        $this->assertSame($blob, $blob->setLength(20));
+        $this->assertEquals(20, $blob->getLength());
+    }
+
+    /**
+     * @covers Zend\Db\Sql\Ddl\Column\Blob::getLength
+     * @deprecated
+     */
+    public function testGetLength()
+    {
+        $blob = new Blob('foo', 55);
+        $this->assertEquals(55, $blob->getLength());
+    }
+
+    /**
      * @covers Zend\Db\Sql\Ddl\Column\Blob::getExpressionData
      */
     public function testGetExpressionData()
