f04798141204cc1d2bb53354d3bb7e09bbc829b6
Patrick Guiran <pguiran@prismamedia.com>
2014-09-13 01:44:14 +0200

    Zend\Filter\Word\SeparatorToSeparator constructor argument must be array in order to be instantiate by PluginManager - see AbstractPluginManager::createFromInvokable()

--- library/Zend/Filter/Word/SeparatorToSeparator.php
+++ library/Zend/Filter/Word/SeparatorToSeparator.php
@@ -14,19 +14,18 @@ use Zend\Filter\Exception;
 
 class SeparatorToSeparator extends AbstractFilter
 {
-    protected $searchSeparator = null;
-    protected $replacementSeparator = null;
+    protected $searchSeparator = ' ';
+    protected $replacementSeparator = '-';
 
     /**
      * Constructor
      *
-     * @param  string $searchSeparator      Separator to search for
-     * @param  string $replacementSeparator Separator to replace with
+     * @param  array|Traversable $options
      */
-    public function __construct($searchSeparator = ' ', $replacementSeparator = '-')
-    {
-        $this->setSearchSeparator($searchSeparator);
-        $this->setReplacementSeparator($replacementSeparator);
+    public function __construct($options = null) {
+        if (null !== $options) {
+            $this->setOptions($options);
+        }
     }
 
     /**
