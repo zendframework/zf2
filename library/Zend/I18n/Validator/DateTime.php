zendframework/zf2

commit 1be015b0cc3d845bd5a96ba93a0b9b1638ec90e5

use instanceof to check for IntlException
instanceof even works for PHP < 5.5 where IntlException does not exist,
so use it instead of get_class().

library/Zend/I18n/Validator/DateTime.php
@@ -271,7 +271,7 @@ class DateTime extends AbstractValidator
                 throw new ValidatorException\InvalidArgumentException($formatter->getErrorMessage());
             }
         } catch(\Exception $e) {
-            if(get_class($e) != 'IntlException'){
+            if($e instanceof IntlException){
                 throw $e;
             }
 
@@ -287,7 +287,7 @@ class DateTime extends AbstractValidator
                 return false;
             }
         } catch(\Exception $e) {
-            if(get_class($e) != 'IntlException'){
+            if($e instanceof IntlException){
                 throw $e;
             }
 

library/Zend/I18n/Validator/Float.php
@@ -127,7 +127,7 @@ class Float extends AbstractValidator
                 throw new Exception\InvalidArgumentException($formatter->getErrorMessage());
             }
         } catch(\Exception $e) {
-            if(get_class($e) != 'IntlException'){
+            if($e instanceof IntlException){
                 throw $e;
             }
 

library/Zend/I18n/Validator/Int.php
@@ -114,7 +114,7 @@ class Int extends AbstractValidator
                 throw new Exception\InvalidArgumentException("Invalid locale string given");
             }
         } catch(\Exception $e) {
-            if(get_class($e) != 'IntlException'){
+            if($e instanceof IntlException){
                 throw $e;
             }
 
@@ -128,7 +128,7 @@ class Int extends AbstractValidator
                 return false;
             }
         } catch(\Exception $e) {
-            if(get_class($e) != 'IntlException'){
+            if($e instanceof IntlException){
                 throw $e;
             }
 

commit f4e2ab4cebb883229293ed8bc8f1bf75dc04d47a

fix the wrong check introduced in 1be015b0cc3d845bd5a96ba93a0b9b1638ec90e5

library/Zend/I18n/Validator/DateTime.php
@@ -271,7 +271,7 @@ class DateTime extends AbstractValidator
                 throw new ValidatorException\InvalidArgumentException($formatter->getErrorMessage());
             }
         } catch(\Exception $e) {
-            if($e instanceof IntlException){
+            if(!$e instanceof IntlException){
                 throw $e;
             }
 
@@ -287,7 +287,7 @@ class DateTime extends AbstractValidator
                 return false;
             }
         } catch(\Exception $e) {
-            if($e instanceof IntlException){
+            if(!$e instanceof IntlException){
                 throw $e;
             }
 

library/Zend/I18n/Validator/Float.php
@@ -127,7 +127,7 @@ class Float extends AbstractValidator
                 throw new Exception\InvalidArgumentException($formatter->getErrorMessage());
             }
         } catch(\Exception $e) {
-            if($e instanceof IntlException){
+            if(!$e instanceof IntlException){
                 throw $e;
             }
 

library/Zend/I18n/Validator/Int.php
@@ -114,7 +114,7 @@ class Int extends AbstractValidator
                 throw new Exception\InvalidArgumentException("Invalid locale string given");
             }
         } catch(\Exception $e) {
-            if($e instanceof IntlException){
+            if(!$e instanceof IntlException){
                 throw $e;
             }
 
@@ -128,7 +128,7 @@ class Int extends AbstractValidator
                 return false;
             }
         } catch(\Exception $e) {
-            if($e instanceof IntlException){
+            if(!$e instanceof IntlException){
                 throw $e;
             }
 