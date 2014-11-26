b310253085d9dbd6dc0ef26057337f695b7c834d
Jakob Møbjerg Nielsen <mobjerg@gmail.com>
2014-11-26 12:10:16 +0100

    Test that Zend\Mvc\View\Console\RouteNotFoundStrategy::renderTable() will render a nicely formatted table, when using posix colored strings.

--- tests/ZendTest/Mvc/View/Console/RouteNotFoundStrategyTest.php
+++ tests/ZendTest/Mvc/View/Console/RouteNotFoundStrategyTest.php
@@ -10,6 +10,8 @@ namespace ZendTest\Mvc\View\Console;
 
 use PHPUnit_Framework_TestCase as TestCase;
 use ReflectionClass;
+use Zend\Console\Adapter\Posix;
+use Zend\Console\ColorInterface;
 use Zend\Mvc\View\Console\RouteNotFoundStrategy;
 
 class RouteNotFoundStrategyTest extends TestCase
@@ -32,4 +34,31 @@ class RouteNotFoundStrategyTest extends TestCase
         $result = $method->invokeArgs($this->strategy, array(array(array()), 1, 0));
         $this->assertSame('', $result);
     }
+	
+	/**
+	 * Test that the renderTable() method will display a nicely formatted table
+	 * when using the posix console adapter and colorized strings.
+	 * 
+	 * @group issue-6922
+	 */
+	public function testRenderTableCalculateCorrectStringLengthForPosixColorizedString()
+	{
+		$reflection = new ReflectionClass('Zend\Mvc\View\Console\RouteNotFoundStrategy');
+		$method = $reflection->getMethod('renderTable');
+		$method->setAccessible(true);
+
+		// Colorized route
+		$console = new Posix();
+		$route = $console->colorize('some route', ColorInterface::GREEN);
+		
+		$table[] = array($route, 'A long explanation that will trigger a line break.');
+
+		// 2 table columns in a 60 column terminal
+		$result = $method->invokeArgs($this->strategy, array($table, 2, 60));
+		$expected = "  $route    A long explanation that will trigger a     \n" .
+					"                line break.                                \n";
+		
+		$this->assertSame($expected, $result);
+	}
+
 }
