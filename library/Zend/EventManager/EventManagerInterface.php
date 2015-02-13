fd615cfc3733c2c99c61c28b45b3a38bb8cf6dad
Adam <adam@grabek.io>
2015-02-13 16:24:28 +0100

    Updated annotation for ::trigger
    Updated annotation for ::trigger to allow for EventInterface instance as $target

--- library/Zend/EventManager/EventManagerInterface.php
+++ library/Zend/EventManager/EventManagerInterface.php
@@ -28,7 +28,7 @@ interface EventManagerInterface extends SharedEventManagerAwareInterface
      *
      * Can emulate triggerUntil() if the last argument provided is a callback.
      *
-     * @param  string $event
+     * @param  string|EventInterface $event
      * @param  object|string $target
      * @param  array|object $argv
      * @param  null|callable $callback
