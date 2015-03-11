3f644ad32c0c2ef7a398c5f8882ffa4fc7fe789f
rms230 <rob@roguewars.co.uk>
2015-03-11 15:16:43 +0000

    Update FormCheckbox.php

--- library/Zend/Form/View/Helper/FormCheckbox.php
+++ library/Zend/Form/View/Helper/FormCheckbox.php
@@ -60,7 +60,7 @@ class FormCheckbox extends FormInput
             $hiddenAttributes = array(
                 'name'  => $attributes['name'],
                 'value' => $element->getUncheckedValue(),
-	        'disabled' => isset($attributes['disabled']) ? $attributes['disabled'] : false,
+                'disabled' => isset($attributes['disabled']) ? $attributes['disabled'] : false,
             );
 
             $rendered = sprintf(
