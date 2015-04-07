077bc8254db30ea0396c31709c904597fed849fb
Romulo Busatto <busatto.romulo@gmail.com>
2015-04-07 09:00:53 -0300

    Create RowCounter.php
    Class for count the register of the one object select

---
+++ library/Zend/Db/Adapter/Driver/Oci8/Feature/RowCounter.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Db\Adapter\Driver\Oci8\Feature;
+
+use Zend\Db\Adapter\Driver\Feature\AbstractFeature;
+use Zend\Db\Adapter\Driver\Oci8\Statement;
+
+/**
+ * RowCounter
+ */
+class RowCounter extends AbstractFeature
+{
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'RowCounter';
+    }
+
+    /**
+     * @param \Zend\Db\Adapter\Driver\Oci8\Statement $statement
+     * @return int
+     */
+    public function getCountForStatement(Statement $statement)
+    {        
+        $countStmt = clone $statement;
+        
+        $sql = $statement->getSql();
+        if ($sql == '' || stripos(strtolower($sql), 'select') === false) {
+            return null;
+        }
+        $countSql = 'SELECT COUNT(*) as "count" FROM (' . $sql . ')';
+        $countStmt->prepare($countSql);        
+        $result = $countStmt->execute();
+        $countRow = $result->current();
+        unset($statement, $result);
+        return $countRow['count'];
+    }
+
+    /**
+     * @param $sql
+     * @return null|int
+     */
+    public function getCountForSql($sql)
+    {              
+        if (stripos(strtolower($sql), 'select') === false) {       
+            return null;
+        }    
+        debug($sql);
+        $countSql = 'SELECT COUNT(*) as "count" FROM (' . $sql . ')';        
+        $result = $this->driver->getConnection()->execute($countSql);
+        $countRow = $result->current();        
+        return $countRow['count'];
+    }
+
+    /**
+     * @param $context
+     * @return closure
+     */
+    public function getRowCountClosure($context)
+    {        
+        $rowCounter = $this;         
+        return function () use ($rowCounter, $context) {              
+            /** @var $rowCounter RowCounter */         
+            return ($context instanceof Statement)
+                ? $rowCounter->getCountForStatement($context)
+                : $rowCounter->getCountForSql($context);
+        };
+    }
+
+}
