2ba5b4d132eff189ded183eb50b67fbbf2cfc937
Romulo Busatto <busatto.romulo@gmail.com>
2015-04-14 09:24:45 -0300

    Returned param $generatedValue

--- library/Zend/Db/Adapter/Driver/Oci8/Result.php
+++ library/Zend/Db/Adapter/Driver/Oci8/Result.php
@@ -62,15 +62,17 @@ class Result implements Iterator, ResultInterface
     /**
      * Initialize
      * @param resource $resource
+     * @param null|int $generatedValue
      * @param null|int $rowCount
      * @return Result
      */
-    public function initialize($resource, $rowCount = null)
+    public function initialize($resource, $generatedValue = null, $rowCount = null)
     {
         if (!is_resource($resource) && get_resource_type($resource) !== 'oci8 statement') {
             throw new Exception\InvalidArgumentException('Invalid resource provided.');
         }
         $this->resource = $resource;
+        $this->generatedValue = $generatedValue;
         $this->rowCount = $rowCount;
         return $this;
     }
